228fa0c2a26b2e3bd77b71cd07b5c91f34c7e201
Petar Veličković <pv273@cam.ac.uk>
2017-12-21 00:46:59 +0100

    Tabs to spaces

--- aop-01.pl
+++ aop-01.pl
@@ -5,7 +5,7 @@ use warnings;
 my $fname = 'input.txt';
 
 open(my $fh, '<', $fname)
-	or die "Could not open file '$fname' $!";
+    or die "Could not open file '$fname' $!";
 
 # Get the number from the file (as a string)
 my $num = <$fh>;
@@ -18,9 +18,9 @@ my @digits = map(substr($num, $_, 1), 0 .. length($num) - 1);
 my $ret = 0;
 
 for (my $i = 0; $i < @digits; $i++) {
-	if ($digits[$i] == $digits[($i + 1) % @digits]) {
-		$ret += $digits[$i];
-	}
+    if ($digits[$i] == $digits[($i + 1) % @digits]) {
+        $ret += $digits[$i];
+    }
 }
 
 print("The captcha solution is ", $ret, "\n");
@@ -30,9 +30,9 @@ my $gap = @digits / 2;
 $ret = 0;
 
 for (my $i = 0; $i < @digits; $i++) {
-	if ($digits[$i] == $digits[($i + $gap) % @digits]) {
-		$ret += $digits[$i];
-	}
+    if ($digits[$i] == $digits[($i + $gap) % @digits]) {
+        $ret += $digits[$i];
+    }
 }
 
 print("The new captcha solution is ", $ret, "\n");
