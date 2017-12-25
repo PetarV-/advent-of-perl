61e2ae32e4cd1f36683d7d3b2c8bd2c59f559049
Petar Veličković <pv273@cam.ac.uk>
2017-12-25 23:45:49 +0100

    Day 9 solved - regex magic.

---
+++ aop-09.pl
@@ -0,0 +1,40 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+
+my $fname = 'input.txt';
+
+open(my $fh, '<', $fname)
+    or die "Could not open file '$fname' $!";
+
+my $ret = 0;
+my $dt = 0;
+
+# Unset the input record separator ($/) to read in entire file
+local $/ = undef;
+
+# Here comes the crazy regex :)
+# Explanation: 
+#  Group := { Content }
+#  Content := (Group | Gibberish | Garbage) Content
+#  Gibberish := (anything except '{', '}' or '<')
+#  Garbage := < GarbageContent >
+#  GarbageContent := (Bang | GarbageGibberish) GarbageContent
+#  Bang := ! (any char)
+#  GarbageGibberish := (anything except '>')
+<$fh> =~ /({(?{$ret += ++$dt})((?1)|[^{}<]*|<(!.|[^>])*>)*}(?{$dt--}))/;
+
+print("The total score is $ret\n");
+
+# Seek back to beginnign of file
+seek($fh, 0, 0);
+
+$ret = 0;
+
+# Another loony regex.
+# Same gist as before, only now we count how many times we hit GarbageGibberish.
+<$fh> =~ /({((?1)|[^{}<]*|<(!.|[^>](?{$ret++}))*>)*})/;
+
+print("The number of non-canceled garbage characters is $ret\n");
+
+close($fh);
